2ca3df4f28df1fd3da6c2735921eb38639a69d38
Christoph Kappestein <k42b3.x@gmail.com>
2016-06-26 21:04:05 +0200

    fix app show dialog

--- app/app/developer/developer.js
+++ app/app/developer/developer.js
@@ -18,11 +18,13 @@ angular.module('fusioApp.app.developer', ['ngRoute'])
         return;
     }
 
-    $http.get(fusio_url + 'consumer/app/developer').then(function(response){
-        $scope.apps = response.data.entry;
-    });
+    $scope.load = function(){
+        $http.get(fusio_url + 'consumer/app/developer').then(function(response){
+            $scope.apps = response.data.entry;
+        });
+    };
 
-    $scope.openCreateDialog = function(){
+    $scope.createApp = function(){
         var modalInstance = $uibModal.open({
             size: 'lg',
             backdrop: 'static',
@@ -31,17 +33,75 @@ angular.module('fusioApp.app.developer', ['ngRoute'])
         });
 
         modalInstance.result.then(function(response){
-            $scope.response = response;
+            $scope.response = response.data;
             $scope.load();
         }, function(){
         });
     };
 
+    $scope.showApp = function(app){
+        var modalInstance = $uibModal.open({
+            size: 'lg',
+            backdrop: 'static',
+            templateUrl: 'app/app/developer/detail.html',
+            controller: 'AppDeveloperDetailCtrl',
+            resolve: {
+                app: function(){
+                    return app;
+                }
+            }
+        });
+
+        modalInstance.result.then(function(response){
+            $scope.response = response.data;
+            $scope.load();
+        }, function(){
+        });
+    };
+
+    $scope.deleteApp = function(appId){
+        
+    };
+
+    $scope.load();
+
 }])
 
 .controller('AppDeveloperCreateCtrl', ['$scope', '$http', '$uibModalInstance', function($scope, $http, $uibModalInstance){
 
+    $scope.app = {
+        name: '',
+        url: '',
+        scopes: []
+    };
+    $scope.scopes = [];
 
+    $http.get(fusio_url + 'consumer/scope').then(function(response){
+        $scope.scopes = response.data.entry;
+    });
+    
+    $scope.create = function(apps){
+        
+    };
+
+    $scope.close = function(){
+        $uibModalInstance.dismiss('cancel');
+    };
+
+}])
+
+.controller('AppDeveloperDetailCtrl', ['$scope', '$http', '$uibModalInstance', 'app', function($scope, $http, $uibModalInstance, app){
+
+    $scope.app = app;
+
+    $http.get(fusio_url + 'consumer/app/developer/' + app.id).then(function(response){
+        $scope.app = response.data;
+    });
+    
+    $scope.close = function(){
+        $uibModalInstance.dismiss('cancel');
+    };
 
 }]);
+
 
