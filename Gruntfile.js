1a96619607f5be2f8d63a29f8841f2f7251b0ce1
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-06-02 09:46:36 +0200

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,63 @@
-module.exports = function(grunt) {
-
+module.exports = function(grunt){
   grunt.initConfig({
     concat: {
-      options: {
-        separator: ';\n',
-        process: function(src, filepath) {
-          return '// Source: ' + filepath + '\n' +
-            src.replace(/\/\/# sourceMappingURL=([A-z0-9\-\.\_]+)/g, '').trim();
-        }
-      },
-      dist: {
+      dist_js: {
+        options: {
+          separator: ';\n',
+          process: function(src, filepath) {
+            return '// Source: ' + filepath + '\n' +
+                src.replace(/\/\/# sourceMappingURL=([A-z0-9\-\.\_]+)/g, '').trim();
+          }
+        },
         src: [
-          './bower_components/angular/angular.min.js',
-          './bower_components/angular-route/angular-route.min.js',
-          './bower_components/angular-sanitize/angular-sanitize.min.js',
-          './bower_components/angular-no-captcha/build/angular-no-captcha.min.js',
-          './bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-          './bower_components/satellizer/satellizer.min.js',
-          './bower_components/angular-gravatar/build/angular-gravatar.min.js',
-          './dist/fusio-app.min.js',
-          './dist/fusio-templates.min.js'
+          './dist/fusio-bundle.min.js',
+          './dist/fusio-templates.js'
         ],
         dest: './dist/fusio.min.js'
+      },
+      dist_css: {
+        options: {
+          separator: '\n',
+          process: function(src, filepath) {
+            return '/* Source: ' + filepath + ' */\n' +
+                src.replace(/\/\/# sourceMappingURL=([A-z0-9\-\.\_]+)/g, '').trim();
+          }
+        },
+        src: [
+          './css/bootstrap.min.css',
+          './css/bootstrap-theme.min.css',
+          './css/ionicons.min.css',
+          './css/default.css'
+        ],
+        dest: './dist/fusio.min.css'
       }
     },
     uglify: {
       options: {
-        banner: '/*\n fusio\n Copyright (C) 2015-2016 Christoph Kappestein\n License: AGPLv3\n*/\n',
+        banner: '/*\n fusio\n Copyright (C) 2015-2018 Christoph Kappestein\n License: AGPLv3\n*/\n',
         mangle: false
       },
       dist: {
         files: {
-          './dist/fusio-app.min.js': [
-            './app/app.js',
-            './app/account/app/developer/developer.js',
-            './app/account/app/grant/grant.js',
-            './app/account/profile/profile.js',
-            './app/account/security/security.js',
-            './app/api/api.js',
-            './app/auth/auth.js',
-            './app/documentation/documentation.js',
-            './app/login/login.js',
-            './app/logout/logout.js',
-            './app/overview/overview.js',
-            './app/signup/signup.js'
+          './dist/fusio-bundle.min.js': [
+            './dist/fusio-bundle.js'
           ]
         }
       }
     },
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1,
-        rebase: ''
-      },
+    browserify: {
       dist: {
         files: {
-          './dist/fusio.min.css': [
-            './bower_components/bootstrap/dist/css/bootstrap.css',
-            './bower_components/bootstrap/dist/css/bootstrap-theme.css',
-            './bower_components/Ionicons/css/ionicons.min.css',
-            './css/default.css'
+          './dist/fusio-bundle.js': [
+            './app/app.js'
           ]
         }
       }
     },
     ngtemplates: {
       fusioApp: {
-        src: 'app/**/*.html',
-        dest: 'dist/fusio-templates.min.js',
+        src: ['app/*/*.html', 'app/*/*/*.html'],
+        dest: 'dist/fusio-templates.js',
         options: {
           htmlmin: {
             collapseBooleanAttributes: true,
@@ -87,9 +76,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-angular-templates');
+  grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', ['uglify', 'ngtemplates', 'concat', 'cssmin']);
+  grunt.registerTask('default', ['browserify', 'ngtemplates', 'uglify', 'concat']);
 
 };
