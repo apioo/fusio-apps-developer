b865be787a98aaabccda23d9a42b5fb09cbc917f
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-01-12 18:36:53 +0100

    update dist

--- dist/fusio-bundle.js
+++ dist/fusio-bundle.js
@@ -1056,6 +1056,7 @@ angular.module('fusioApp.logout', ['ngRoute'])
       $auth.logout()
       $rootScope.isAuthenticated = $auth.isAuthenticated()
       $rootScope.userName = null
+      $rootScope.account = null
     }
 
     $location.url('/login')
