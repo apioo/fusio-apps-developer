0679510583219497b73fdaac3db8de18c848f2e0
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-08-17 16:34:45 +0200

    add password folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,17 +16,17 @@ import {ContractsComponent} from './account/contracts/contracts.component';
 import {InvoicesComponent} from './account/invoices/invoices.component';
 import {GrantsComponent} from "./account/grants/grants.component";
 import {AppsComponent} from "./account/apps/apps.component";
-import { NavigationComponent } from './navigation/navigation.component';
+import {NavigationComponent} from './navigation/navigation.component';
 import {GravatarModule} from "ngx-gravatar";
-import { ApiComponent } from './api/api.component';
-import { BootstrapComponent } from './bootstrap/bootstrap.component';
-import { SdkComponent } from './sdk/sdk.component';
-import { SupportComponent } from './support/support.component';
-import { RegisterComponent } from './register/register.component';
-import { ActivateComponent } from './activate/activate.component';
-import { AuthComponent } from './auth/auth.component';
-import { ConfirmComponent } from './confirm/confirm.component';
-import { ResetComponent } from './reset/reset.component';
+import {ApiComponent} from './api/api.component';
+import {BootstrapComponent} from './bootstrap/bootstrap.component';
+import {SdkComponent} from './sdk/sdk.component';
+import {SupportComponent} from './support/support.component';
+import {RegisterComponent} from './register/register.component';
+import {ActivateComponent} from './activate/activate.component';
+import {AuthComponent} from './auth/auth.component';
+import {ConfirmComponent} from './password/confirm/confirm.component';
+import {ResetComponent} from './password/reset/reset.component';
 
 @NgModule({
   declarations: [
