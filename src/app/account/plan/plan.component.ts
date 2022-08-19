07b43182170fd57c9160f32b2b0646f54eb25b08
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-08-19 20:14:48 +0200

    add plan

--- src/app/account/plan/plan.component.ts
+++ src/app/account/plan/plan.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from "../../client.service";
+import {Plan} from "fusio-sdk/dist/src/generated/consumer/Plan";
+import {LocationStrategy} from "@angular/common";
 
 @Component({
   selector: 'app-account-plan',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanComponent implements OnInit {
 
-  constructor() { }
+  plans?: Array<Plan>
 
-  ngOnInit(): void {
+  private provider = 'stripe';
+
+  constructor(private client: ClientService, private location: LocationStrategy) { }
+
+  async ngOnInit(): Promise<void> {
+    const group = await this.client.getClient().consumerPlan();
+    const response = await group.getConsumerPlan().consumerActionPlanGetAll({count: 1024});
+    this.plans = response.data.entry;
+  }
+
+  async doBillingPortal() {
+    const group = await this.client.getClient().consumerPayment();
+    const response = await group.getConsumerPaymentByProviderPortal(this.provider).consumerActionPaymentPortal();
+
+    if (response.data.redirectUrl) {
+      location.href = response.data.redirectUrl;
+    }
+  }
+
+  async doPurchase(plan: Plan) {
+    const group = await this.client.getClient().consumerPayment();
+    const response = await group.getConsumerPaymentByProviderCheckout(this.provider).consumerActionPaymentCheckout({
+      planId: plan.id,
+      returnUrl: this.location.prepareExternalUrl('/account'),
+    });
+
+    if (response.data.approvalUrl) {
+      location.href = response.data.approvalUrl;
+    }
   }
 
 }
