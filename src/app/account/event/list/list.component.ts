a059b4bd63084e25a18f369dcf48ce3819ee46ba
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-08-19 18:39:16 +0200

    update event

--- src/app/account/event/list/list.component.ts
+++ src/app/account/event/list/list.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {List} from "ngx-fusio-sdk";
+import Client from "fusio-sdk/dist/src/generated/consumer/Client";
+import {Collection_Query} from "fusio-sdk/dist/src/generated/consumer/Collection_Query";
+import {AxiosResponse} from "axios";
+import {Collection} from "fusio-sdk/dist/src/generated/consumer/Collection";
+import {ModalComponent} from "../modal/modal.component";
+import {Event_Subscription} from "fusio-sdk/dist/src/generated/consumer/Event_Subscription";
 
 @Component({
   selector: 'app-account-event-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent extends List<Client, Event_Subscription> {
 
-  constructor() { }
+  protected async getAll(query: Collection_Query): Promise<AxiosResponse<Collection<Event_Subscription>>> {
+    const group = await this.factory.getClient().consumerSubscription();
+    return await group.getConsumerSubscription().consumerActionEventSubscriptionGetAll(query);
+  }
 
-  ngOnInit(): void {
+  protected async get(id: string): Promise<AxiosResponse<Event_Subscription>> {
+    const group = await this.factory.getClient().consumerSubscription();
+    return await group.getConsumerSubscriptionBySubscriptionId(id).consumerActionEventSubscriptionGet();
+  }
+
+  protected getDetailComponent(): any {
+    return ModalComponent;
+  }
+
+  protected getRoute(): any {
+    return '/account/event';
   }
 
 }
--- src/app/account/event/modal/modal.component.ts
+++ src/app/account/event/modal/modal.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Event} from "fusio-sdk/dist/src/generated/consumer/Event";
+import {AxiosResponse} from "axios";
+import {Message} from "fusio-sdk/dist/src/generated/consumer/Message";
+import {Modal} from "ngx-fusio-sdk";
+import Client from "fusio-sdk/dist/src/generated/consumer/Client";
+import {Event_Subscription} from "fusio-sdk/dist/src/generated/consumer/Event_Subscription";
+import {Event_Subscription_Create} from "fusio-sdk/dist/src/generated/consumer/Event_Subscription_Create";
+import {Event_Subscription_Update} from "fusio-sdk/dist/src/generated/consumer/Event_Subscription_Update";
 
 @Component({
   selector: 'app-account-event-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent extends Modal<Client, Event_Subscription> {
 
-  constructor() { }
+  events?: Array<Event>;
 
-  ngOnInit(): void {
+  override async ngOnInit(): Promise<void> {
+    const group = await this.factory.getClient().consumerEvent();
+    const response = await group.getConsumerEvent().consumerActionEventGetAll({count: 1024});
+    this.events = response.data.entry;
+  }
+
+  protected async create(entity: Event_Subscription): Promise<AxiosResponse<Message>> {
+    const group = await this.factory.getClient().consumerSubscription();
+    return await group.getConsumerSubscription().consumerActionEventSubscriptionCreate(<Event_Subscription_Create> entity);
+  }
+
+  protected async update(entity: Event_Subscription): Promise<AxiosResponse<Message>> {
+    const group = await this.factory.getClient().consumerSubscription();
+    return await group.getConsumerSubscriptionBySubscriptionId('' + entity.id).consumerActionEventSubscriptionUpdate(<Event_Subscription_Update> entity);
+  }
+
+  protected async delete(entity: Event_Subscription): Promise<AxiosResponse<Message>> {
+    const group = await this.factory.getClient().consumerSubscription();
+    return await group.getConsumerSubscriptionBySubscriptionId('' + entity.id).consumerActionEventSubscriptionDelete();
+  }
+
+  protected newEntity(): Event_Subscription {
+    return {
+      event: '',
+      endpoint: '',
+    };
   }
 
 }
